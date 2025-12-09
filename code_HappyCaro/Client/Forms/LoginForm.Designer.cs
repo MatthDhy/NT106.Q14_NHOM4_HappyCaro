131606cc9f751f8b77debc3055090a96e3a8232a
HoangPhu <hoangphu140906@gmail.com>
2025-12-09 11:21:22 +0700

    Modify login form

--- code_HappyCaro/Client/Forms/LoginForm.Designer.cs
+++ code_HappyCaro/Client/Forms/LoginForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace Client.Forms
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Client.Forms
 {
     partial class LoginForm
     {
@@ -28,12 +31,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblAsk = new System.Windows.Forms.Label();
+            this.lnkForgotPass = new System.Windows.Forms.LinkLabel();
+            this.lnkRegister = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(273, 40);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(260, 54);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Đăng nhập";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(240, 135);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(329, 38);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Text = "Tên đăng nhập";
+
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(240, 198);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(329, 38);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "Mật khẩu";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(240, 297);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(182, 42);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblAsk
+            // 
+            this.lblAsk.AutoSize = true;
+            this.lblAsk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsk.Location = new System.Drawing.Point(270, 393);
+            this.lblAsk.Name = "lblAsk";
+            this.lblAsk.Size = new System.Drawing.Size(152, 20);
+            this.lblAsk.TabIndex = 5;
+            this.lblAsk.Text = "Chưa có tài khoản?";
+            // 
+            // lnkForgotPass
+            // 
+            this.lnkForgotPass.ActiveLinkColor = System.Drawing.Color.DeepSkyBlue;
+            this.lnkForgotPass.AutoSize = true;
+            this.lnkForgotPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkForgotPass.LinkColor = System.Drawing.Color.Black;
+            this.lnkForgotPass.Location = new System.Drawing.Point(424, 260);
+            this.lnkForgotPass.Name = "lnkForgotPass";
+            this.lnkForgotPass.Size = new System.Drawing.Size(145, 20);
+            this.lnkForgotPass.TabIndex = 8;
+            this.lnkForgotPass.TabStop = true;
+            this.lnkForgotPass.Text = "Quên mật khẩu?";
+            // 
+            // lnkRegister
+            // 
+            this.lnkRegister.ActiveLinkColor = System.Drawing.Color.DeepSkyBlue;
+            this.lnkRegister.AutoSize = true;
+            this.lnkRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkRegister.LinkColor = System.Drawing.Color.Black;
+            this.lnkRegister.Location = new System.Drawing.Point(428, 393);
+            this.lnkRegister.Name = "lnkRegister";
+            this.lnkRegister.Size = new System.Drawing.Size(76, 20);
+            this.lnkRegister.TabIndex = 9;
+            this.lnkRegister.TabStop = true;
+            this.lnkRegister.Text = "Đăng ký";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(829, 459);
+            this.Controls.Add(this.lnkRegister);
+            this.Controls.Add(this.lnkForgotPass);
+            this.Controls.Add(this.lblAsk);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblLogin);
+            this.Name = "LoginForm";
             this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblAsk;
+        private System.Windows.Forms.LinkLabel lnkForgotPass;
+        private LinkLabel lnkRegister;
     }
 }
--- code_HappyCaro/Client/Forms/LoginForm.cs
+++ code_HappyCaro/Client/Forms/LoginForm.cs
@@ -16,5 +16,65 @@ namespace Client.Forms
         {
             InitializeComponent();
         }
+        private void RemovePlaceholderEvent(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            if (txt.ForeColor == Color.Gray)
+            {
+                txt.Text = "";
+                txt.ForeColor = Color.Black;
+            }
+        }
+        private void AddPlaceholderEvent(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            if (string.IsNullOrWhiteSpace(txt.Text))
+            {
+                SetPlaceholder(txt, "Tên đăng nhập");
+            }
+        }
+        private void RemovePasswordPlaceholderEvent(object sender, EventArgs e)
+        {
+            if (txtPassword.ForeColor == Color.Gray)
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.Black;
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+        private void AddPasswordPlaceholderEvent(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                txtPassword.UseSystemPasswordChar = false;
+                SetPlaceholder(txtPassword, "Mật khẩu");
+            }
+        }
+        private void SetPlaceholder(TextBox txt, string text)
+        {
+            txt.Text = text;
+            txt.ForeColor = Color.Gray;
+        }
+        
+        
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            SetPlaceholder(txtUsername, "Tên đăng nhập");
+            SetPlaceholder(txtPassword, "Mật khẩu");
+
+            // Gán sự kiện
+            txtUsername.Enter += RemovePlaceholderEvent;
+            txtUsername.Leave += AddPlaceholderEvent;
+
+            txtPassword.Enter += RemovePasswordPlaceholderEvent;
+            txtPassword.Leave += AddPasswordPlaceholderEvent;
+
+            this.ActiveControl = lblLogin;
+        }
+
+        
     }
 }
