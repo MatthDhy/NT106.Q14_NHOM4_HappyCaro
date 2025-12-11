209f8197e164cfb6de01453f064b0189a943da07
DHuyyy <24520656@gm.uit.edu.vn>
2025-12-11 10:29:23 +0700

    update client

---
+++ code_HappyCaro/Client/ClientDispatcher.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Client
+{
+    public class ClientDispatcher
+    {
+        private readonly TcpClientHelper _tcp;
+
+        public ClientDispatcher(TcpClientHelper tcp)
+        {
+            _tcp = tcp;
+            _tcp.OnEnvelopeReceived += Dispatch;
+        }
+
+        private void Dispatch(MessageEnvelope env)
+        {
+            switch (env.Type)
+            {
+                // AUTH ###################################################
+                case MessageType.AUTH_LOGIN_OK:
+                    OnLoginSuccess?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.AUTH_LOGIN_FAIL:
+                    OnLoginFail?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.AUTH_REGISTER_OK:
+                    OnRegisterSuccess?.Invoke();
+                    break;
+
+                case MessageType.AUTH_REGISTER_FAIL:
+                    OnRegisterFail?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.AUTH_RESET_OK:
+                    OnResetRequested?.Invoke();
+                    break;
+
+                case MessageType.AUTH_RESET_VERIFY_OK:
+                    OnResetSuccess?.Invoke();
+                    break;
+
+                case MessageType.AUTH_RESET_VERIFY_FAIL:
+                    OnResetFail?.Invoke();
+                    break;
+
+                // ROOM ###################################################
+                case MessageType.ROOM_LIST:
+                    OnRoomList?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.ROOM_CREATE_OK:
+                    OnRoomCreated?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.ROOM_JOIN_OK:
+                    OnRoomJoined?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.ROOM_JOIN_FAIL:
+                    OnRoomJoinFail?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.ROOM_UPDATE:
+                    OnRoomUpdate?.Invoke(env.Payload);
+                    break;
+
+                // GAME ###################################################
+                case MessageType.GAME_UPDATE:
+                    OnGameUpdate?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.GAME_END:
+                    OnGameEnd?.Invoke(env.Payload);
+                    break;
+
+                case MessageType.GAME_SURRENDER_RECV:
+                    OnOpponentSurrender?.Invoke();
+                    break;
+
+                // CHAT ###################################################
+                case MessageType.CHAT_RECV:
+                    OnChatReceived?.Invoke(env.Payload);
+                    break;
+
+                // RANK ##################################################
+                case MessageType.RANKING_DATA:
+                    OnRankingReceived?.Invoke(env.Payload);
+                    break;
+
+                // ERROR #################################################
+                case MessageType.ERROR:
+                    OnError?.Invoke(env.Payload);
+                    break;
+            }
+        }
+
+        // AUTH EVENTS
+        public event Action<string> OnLoginSuccess;
+        public event Action<string> OnLoginFail;
+        public event Action OnRegisterSuccess;
+        public event Action<string> OnRegisterFail;
+        public event Action OnResetRequested;
+        public event Action OnResetSuccess;
+        public event Action OnResetFail;
+
+        // ROOM EVENTS
+        public event Action<string> OnRoomList;
+        public event Action<string> OnRoomCreated;
+        public event Action<string> OnRoomJoined;
+        public event Action<string> OnRoomJoinFail;
+        public event Action<string> OnRoomUpdate;
+
+        // GAME EVENTS
+        public event Action<string> OnGameUpdate;
+        public event Action<string> OnGameEnd;
+        public event Action OnOpponentSurrender;
+
+        // CHAT
+        public event Action<string> OnChatReceived;
+
+        // RANKING
+        public event Action<string> OnRankingReceived;
+
+        // ERROR
+        public event Action<string> OnError;
+    }
+}
---
+++ code_HappyCaro/Client/ClientRequest.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+
+namespace Client
+{
+    public class ClientRequest
+    {
+        private readonly TcpClientHelper _tcp;
+
+        public ClientRequest(TcpClientHelper tcp)
+        {
+            _tcp = tcp;
+        }
+
+        private void Send(MessageType type, object payload)
+        {
+            _tcp.EnqueueSend(new MessageEnvelope
+            {
+                Type = type,
+                Payload = JsonSerializer.Serialize(payload)
+            });
+        }
+
+        // AUTH ==============================================================
+        public void Login(string username, string password)
+            => Send(MessageType.AUTH_LOGIN, new { username, password });
+
+        public void Register(string username, string password)
+            => Send(MessageType.AUTH_REGISTER, new { username, password });
+
+        public void ForgotPassword(string email)
+            => Send(MessageType.AUTH_RESET_REQUEST, new { Email = email });
+
+        public void VerifyReset(string email, string token, string newPassword)
+            => Send(MessageType.AUTH_RESET_VERIFY, new { Email = email, Token = token, NewPassword = newPassword });
+
+        public void Logout()
+            => Send(MessageType.AUTH_LOGOUT, new { });
+
+        // ROOM ==============================================================
+        public void CreateRoom()
+            => Send(MessageType.ROOM_CREATE, new { });
+
+        public void JoinRoom(int roomId)
+            => Send(MessageType.ROOM_JOIN, new { RoomId = roomId });
+
+        public void LeaveRoom()
+            => Send(MessageType.ROOM_LEAVE, new { });
+
+        public void RequestRoomList()
+            => Send(MessageType.ROOM_LIST, new { });
+
+        // GAME ==============================================================
+        public void SendMove(int x, int y)
+            => Send(MessageType.GAME_MOVE, new { x, y });
+
+        public void Surrender()
+            => Send(MessageType.GAME_SURRENDER, new { });
+
+        // CHAT ==============================================================
+        public void Chat(string message)
+            => Send(MessageType.CHAT_SEND, new { message });
+
+        // RANKING ============================================================
+        public void RequestRanking()
+            => Send(MessageType.REQUEST_RANKING, new { });
+    }
+}
